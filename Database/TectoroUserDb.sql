e27ebb2a32dc21aa3464eea524444eacdb566e64
intenseNibo <nabodhe@yahoo.com>
2020-11-25 06:10:47 +0530

    added db scriptAdd project files.

---
+++ Database/TectoroUserDb.sql
@@ -0,0 +1,45 @@
+﻿If Not Exists (Select * from sys.schemas where name = '[Domain]')
+      Begin
+	    If Exists (Select * from sys.tables where name = 'Users')
+		Begin 
+			Drop Table [Domain].[Users];
+			print ' Table deleted.'
+		End
+
+		drop Schema [Domain];
+		print ' Schema deleted.'
+	  End
+
+Go
+
+create Schema [Domain];
+
+Go
+print ' Schema created.'
+Go
+
+CREATE TABLE [Domain].[Users] (
+
+[UserId] INT IDENTITY (1, 1) NOT NULL,
+
+[UserName] NVARCHAR (200) NOT NULL,
+
+[Email] NVARCHAR (1000) NULL,
+
+[Alias] NVARCHAR (1000) NULL,
+
+[FirstName] NVARCHAR (1000) NULL,
+
+[LastName] NVARCHAR (1000) NULL,
+
+[Position] NVARCHAR (200) NULL,
+
+[Level] Int NULL,
+
+[ManagerId] Int NULL,
+
+CONSTRAINT [PK_Domain.Users] PRIMARY KEY CLUSTERED ([UserId] ASC))
+
+print ' Table created.'
+
+Go
